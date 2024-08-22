f94662b67540e831e1e4aed5e7da44f206bd920f
Ihor Tresnystkyi <ihortresnystkyi@Ihors-MacBook-Pro.local>
2024-08-22 14:05:27 +0100

    Updated method get_db_connection in src/utils

--- src/utils/connection_db.py
+++ src/utils/connection_db.py
@@ -1,16 +1,25 @@
+import logging.config
+import os
+
 import psycopg2
 
-from src.config.settings import ConfigDatabase
+from src.config.log_config import LOGGING
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
 
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        host=ConfigDatabase.HOST,
-        database=ConfigDatabase.DB,
-        user=ConfigDatabase.USER,
-        password=ConfigDatabase.PASSWORD
-    )
-    return conn
+    try:
+        database_url = os.getenv('DATABASE_URL')
+        if not database_url:
+            raise ValueError("DATABASE_URL environment variable is not set")
+        conn = psycopg2.connect(database_url)
+        logger.info(f"Successfully connected to database")
+        return conn
+    except (psycopg2.Error, ValueError) as e:
+        logger.error(f"Unable to connect to the database. Error: {e}")
+        raise
 
 
 def get_top_inviters(limit=10):
